881b69df75a0222cf7e39e6bdc6352dac3ed4978
Vec7or <carlo@kirchmeier.co>
2020-09-30 19:56:15 +0200

    Allow native implementation

--- vorstudie/forge-2/background.js
+++ vorstudie/forge-2/background.js
@@ -1,5 +1,3 @@
-forge.options.usePureJavaScript = true;
-
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
         console.log("The color is green.");
