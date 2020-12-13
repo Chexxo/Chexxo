e67b9c46a4a9e03a281300c5389d759a35e4dee7
Vec7or <carlo@kirchmeier.co>
2020-12-13 16:36:17 +0100

    Add some typedoc

--- src/background/App.ts
+++ src/background/App.ts
@@ -13,6 +13,11 @@ import { UnknownError } from "../types/errors/UnknownError";
 import { InBrowserLogger } from "./logger/InBrowserLogger";
 import { LogEntry } from "../shared/types/logger/LogEntry";
 
+/**
+ * Class which is responsible for combining the quality
+ * and the certificate service. Also this class is used for
+ * global error handling and logging.
+ */
 export class App {
   private tabCache = new Map<number, TabData>();
 
@@ -26,20 +31,43 @@ export class App {
     configurator.addListener(this.updateConfiguration.bind(this));
   }
 
+  /**
+   * Initialites the application by getting the current configuration
+   * of the extension and updating it in all dependent classes.
+   */
   public async init(): Promise<void> {
     const configuration = await this.configurator.getConfiguration();
     this.updateConfiguration(configuration);
   }
 
+  /**
+   * Updates the configuration of all services the app is aware of.
+   *
+   * @param configuration The new configuration to be used.
+   */
   public updateConfiguration(configuration: Configuration): void {
     this.certificateService.updateConfiguration(configuration);
     this.qualityService.updateConfiguration(configuration);
   }
 
+  /**
+   * Resets all saved data for the tab with the given tabId.
+   *
+   * @param tabId Id of the tab which should be reset.
+   */
   public resetTabData(tabId: number): void {
     this.tabCache.delete(tabId);
   }
 
+  /**
+   * Fetches the certificate for the url provided in the request details.
+   * If an error occures it will be logged and the error forwarded to the
+   * frontend. If the certificate could be fetched it will be saved in the
+   * tabCache for the tab affected.
+   *
+   * @param requestDetails The details of the request which lead to this
+   * fetch.
+   */
   public async fetchCertificate(requestDetails: {
     url: string;
     tabId: number;
@@ -69,6 +97,19 @@ export class App {
     this.tabCache.set(tabId, tabData);
   }
 
+  /**
+   * Analyzes the quality of the certifcate cached for the tab specified
+   * within the request details. If the qualiy has not decreased since
+   * the last time the user visited the domain the new quality will be
+   * saved to cache. Should the quality have decreased an error is
+   * written into the tab cache.
+   *
+   * @param requestDetails The details of the request which contains the
+   * tabId of the tab to be analyzed.
+   *
+   * @returns Boolean if the quality of the certificate has decreased since the
+   * last time the user visitied the domain.
+   */
   public async analyzeQuality(requestDetails: {
     tabId: number;
     url: string;
@@ -97,6 +138,15 @@ export class App {
     return false;
   }
 
+  /**
+   * Callback for browser specific errors. Will be called if the browser
+   * throws an error regarding navigation. The error will then be analyzed
+   * and if it is relevant for the extension it will be saved to the tab
+   * cache.
+   *
+   * @param requestDetails The details of the request which lead to the
+   * error.
+   */
   public analyzeError(requestDetails: {
     url: string;
     tabId: number;
@@ -118,37 +168,71 @@ export class App {
     }
   }
 
+  /**
+   * Gets the tab information of the tab id provided.
+   *
+   * @param tabId The id of the tab from which the information shall
+   * be returned.
+   */
   public getTabData(tabId: number): TabData | undefined {
     return this.tabCache.get(tabId);
   }
 
+  /**
+   * See {@link QualityService.resetQuality}
+   */
   public async resetQuality(url: string): Promise<void> {
     await this.qualityService.resetQuality(url);
   }
 
+  /**
+   * See {@link Configurator.getConfiguration}
+   */
   public async getConfiguration(): Promise<Configuration> {
     return await this.configurator.getConfiguration();
   }
 
+  /**
+   * See {@link Configurator.setConfiguration}
+   */
   public async setConfiguration(configuration: Configuration): Promise<void> {
     await this.configurator.setConfiguration(configuration);
   }
 
+  /**
+   * Removes all saved qualities from the domain cache.
+   */
   public async removeCache(): Promise<void> {
     this.logger.log(UUIDFactory.uuidv4(), LogLevel.INFO, `Cache was removed.`);
     return this.qualityService.removeQualities();
   }
 
+  /**
+   * Returns all log entries currently persisted inside the browser storage.
+   */
   public async exportLogs(): Promise<LogEntry[] | null> {
     this.logger.log(UUIDFactory.uuidv4(), LogLevel.INFO, `Logs were exported.`);
     return this.logger.getAll();
   }
 
+  /**
+   * Removes all logs which were persisted inside the browser storage.
+   */
   public async removeLogs(): Promise<void> {
     this.logger.log(UUIDFactory.uuidv4(), LogLevel.INFO, `Logs were removed.`);
     return this.logger.removeAll();
   }
 
+  /**
+   * Takes the given url and error object and creates a new
+   * log entry accordingly. Normally the error object should
+   * be either a {@link CertificateResponse} or a
+   * {@link RawCertificateResponse}. But this function can also
+   * handle other types of error objects.
+   *
+   * @param url The url which can be associated with this error.
+   * @param errorResponse The error object which was thrown.
+   */
   private logError(url: string, errorResponse: unknown) {
     let logLevel = LogLevel.ERROR;
     let responseMessage = "Unknown Error";
--- src/background/quality/helpers/QualityAnalyzer.ts
+++ src/background/quality/helpers/QualityAnalyzer.ts
@@ -1,7 +1,19 @@
 import { Certificate } from "../../../types/certificate/Certificate";
 import { Quality } from "../../../types/Quality";
 
+/**
+ * Class providing functionality regarding the detection
+ * of the quality of a certificate.
+ */
 export abstract class QualityAnalyzer {
+  /**
+   * Detects the quality of the given certificate.
+   *
+   * @param certificate The certificate of which the
+   * quality shall be detected.
+   *
+   * @returns The quality of the certificate given.
+   */
   public static getQuality(certificate: Certificate): Quality {
     if (this.isDomainValidated(certificate)) {
       return Quality.DomainValidated;
@@ -18,14 +30,32 @@ export abstract class QualityAnalyzer {
     return Quality.Unknown;
   }
 
+  /**
+   * Detects if the given certificate is domain validated. This is
+   * done by checking if the certificate contains the correct OID.
+   *
+   * @param certificate The certificate to be checked.
+   */
   private static isDomainValidated(certificate: Certificate): boolean {
     return certificate.certificatePolicies.includes("2.23.140.1.2.1");
   }
 
+  /**
+   * Detects if the given certificate is organization validated. This is
+   * done by checking if the certificate contains the correct OID.
+   *
+   * @param certificate The certificate to be checked.
+   */
   private static isOrganizationValidated(certificate: Certificate): boolean {
     return certificate.certificatePolicies.includes("2.23.140.1.2.2");
   }
 
+  /**
+   * Detects if the given certificate is extended validated. This is
+   * done by checking if the certificate contains the correct OID.
+   *
+   * @param certificate The certificate to be checked.
+   */
   private static isExtendedValidated(certificate: Certificate): boolean {
     return certificate.certificatePolicies.includes("2.23.140.1.1");
   }
